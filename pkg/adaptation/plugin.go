d62d56025d89e67f79c000192b0d0ca64065327b
Krisztian Litkey <krisztian.litkey@intel.com>
2024-08-21 16:57:35 +0300

    adaptation: increase default registration timeout.
    Plugin registration takes three ttrpc requests and may require
    some extra processing of the initial sync message. Hence, give
    plugins a bit more slack to get fully up and running.
    
    Signed-off-by: Krisztian Litkey <krisztian.litkey@intel.com>

--- pkg/adaptation/plugin.go
+++ pkg/adaptation/plugin.go
@@ -38,10 +38,10 @@ import (
 )
 
 const (
-	// DefaultPluginRegistrationTimeout is the default timeout for plugin registration.
-	DefaultPluginRegistrationTimeout = 5 * time.Second
 	// DefaultPluginRequestTimeout is the default timeout for plugins to handle a request.
 	DefaultPluginRequestTimeout = 2 * time.Second
+	// DefaultPluginRegistrationTimeout is the default timeout for plugin registration.
+	DefaultPluginRegistrationTimeout = 2*time.Second + 3*DefaultPluginRequestTimeout
 )
 
 var (
